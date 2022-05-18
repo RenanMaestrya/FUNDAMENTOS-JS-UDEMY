15559405e3f418951107c74315742b0d65628f01
Renan Almeida <renanalmeida2.ra@gmail.com>
2022-05-18 11:46:09 -0300

    adicionado desafio area circunferencia

---
+++ desafioAreaCircunferencia.js
@@ -0,0 +1,6 @@
+const PI = 3.141592; //valor de pi 
+let raio = 12; //valor do raio em m²
+
+let areaCircunferencia = PI * (raio * raio); //calculo da área da circunferencia
+
+console.log(areaCircunferencia + "m²");
