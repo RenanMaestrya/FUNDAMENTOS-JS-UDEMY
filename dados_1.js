fc0881f29af36424312ed13662e0ba52356142bc
Renan Almeida <renanalmeida2.ra@gmail.com>
2022-05-18 11:29:57 -0300

    commit initial

---
+++ dados_1.js
@@ -0,0 +1,12 @@
+var quantidade = 10;
+var preco = 6.4;
+var espessura = 7.8;
+var imposto = 2;
+var valorFinal = preco + imposto;
+
+console.log("caneta")
+console.log(quantidade)
+console.log(preco)
+console.log(espessura)
+
+console.log("o valor final é "+ valorFinal + " R$")
